d5547c7a628c9bd44500c73df10205bac568fb99
Demetrius Berkeley <Dberkeley@fullsail.edu>
2017-08-28 16:50:33 -0400

    Completed html. Working on sass

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+var babel = require("gulp-babel");
+
+gulp.task('sass', function () {
+  console.log('FIRE')
+  return gulp.src('./stylesheet/**/*.sass')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+      stream:true
+    }))
+})
+
+gulp.task('watch', ['browser-sync', 'sass'], 'babel', function () {
+  gulp.watch('./stylesheet/**/*.sass', ['sass'])
+})
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+})
+
+gulp.task('babel', function() {
+    return gulp.src("src/app.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
+})
